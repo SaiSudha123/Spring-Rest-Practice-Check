c9bf3dbeea43ba5ba75dc051e90c9c83f3f5bedb
Sai Sudha <80373665+SaiSudha123@users.noreply.github.com>
2021-04-21 09:11:50 +0530

    Add files via upload

---
+++ truyum/src/main/java/com/cognizant/truyum/service/MenuItemService.java
@@ -0,0 +1,19 @@
+package com.cognizant.truyum.service;
+
+import java.util.List;
+
+import com.cognizant.truyum.exception.GlobalExceptionHandler;
+import com.cognizant.truyum.exception.MenuItemException;
+import com.cognizant.truyum.model.MenuItem;
+
+public interface MenuItemService {
+
+	List<MenuItem> getAllMenuItems();
+	List<MenuItem> getAllMenuItemsAd();
+	MenuItem getMenuItemById(int id) throws MenuItemException;
+//	MenuItem modifyMenuItem(MenuItem menuItem)throws MenuItemException;
+	MenuItem modifyMenuItem(MenuItem menuItem,int id)throws MenuItemException;
+	MenuItem editMenuItem(MenuItem menuItem);
+	
+	
+}
